8245c682daadf327ab2397b1113cb4482ba0d867
Kevin Garcia <kevinede40@gmail.com>
2021-10-22 15:22:47 -0600

    Website Using Bored API

--- boredapi/src/components/Buscador.jsx
+++ boredapi/src/components/Buscador.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useState }  from 'react';
 
 const Buscador = () => {
-/*     const API_URL = 'https://reqres.in/api/users';
-    fetch(API_URL)
-    .then(res => res.json())
-    .then(data => console.log(data)) */
+    const [tarea, setTarea] = useState('');
+    const [type, setType] = useState('');
+    const [link, setLink] = useState('');
+    const [participants, setParticipants] = useState(0);
 
-/*     const API_URL = 'https://www.boredapi.com/api/activity/';
-    fetch(API_URL)
-        .then((response) => response.json())
-        .then((activities) => {console.log(activities); console.log(activities.activity);}) */
-        var prueba = {};
+    function pedirActividad() {
+        setType('Loading ...');
+        setTarea('Loading ...');
+        setLink('Loading ...');
+        setParticipants('Loading ...');
         fetch('https://www.boredapi.com/api/activity/')
-        .then((response) => response.json())
-        .then(activities => {prueba = {activities}})
-        console.log(prueba)
+        .then(response => response.json())
+        .then(activities => {
+            setType(activities.type);
+            setTarea(activities.activity);
+            setParticipants(activities.participants);
+            setLink(activities.link);
+        })
+    };
+    
     return (
-        <div>
+        <div className="d-flex justify-content-center align-items-center flex-column mt-5">
             <h2>Your Activity:</h2>
-            <p>{prueba}</p>
+            <p className="lead text-center">Your task is: <div className="lead text-secondary text-center">{tarea}</div> </p>
+            <p className="lead text-center">Type:<div className="lead text-secondary text-center">{type}</div> </p> 
+            <p className="lead text-center">Link:<div className="lead text-secondary text-center">{link}</div> </p> 
+            <p className="lead text-center">Participants: <div className="lead text-secondary text-center">{participants}</div></p>
+            <button type="button" className="btn btn-dark " onClick={() => pedirActividad()}>Actividad Nueva</button>
         </div>
     )
 }
+
 
 export default Buscador
